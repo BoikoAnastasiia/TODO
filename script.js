56df0df6eff467ac9df0a76b9a9d9aae47dd5c8f
Nastya Boiko <boiko.nastasiia@gmail.com>
2022-10-02 18:26:54 +0400

    save

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const todoBody = document.getElementById('todo_body');
 const addNewTodoButton = document.getElementById('add_new_todo');
 const todoList = document.getElementById('todo_list');
 const modal = document.getElementById('modal_wrapper');
+const form = document.getElementById('modal');
 
 addNewTodoButton.addEventListener('click', () => {
   const headlineTodo = headlineText.value || 'Title';
@@ -62,35 +63,11 @@ function addButtonListener(item) {
   const doneButton = item.querySelector('.finish_button');
   const deleteButton = item.querySelector('.delete_button');
   const editButton = item.querySelector('.edit_button');
-  const TODOHeadline = item.querySelector('h2');
-  const TODOText = item.querySelector('p');
-  const editHeadline = document.getElementById('edit_title');
-  const editText = document.getElementById('edit_todo');
-  const form = document.querySelector('.modal');
 
   const deleteButtonHandler = () => {
     doneButton.removeEventListener('click', changeStatusHandler);
     deleteButton.removeEventListener('click', deleteButtonHandler);
     item.remove();
-  };
-
-  const editButtonHandler = () => {
-    modal.style.display = 'block';
-    editHeadline.value = TODOHeadline.innerHTML;
-    editText.value = TODOText.textContent;
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-
-    const {
-      elements: { editTitle, editTodoText }
-    } = event.currentTarget;
-
-    TODOHeadline.innerHTML = editTitle.value;
-    TODOText.textContent = editTodoText.value;
-    modal.style.display = 'none';
-    event.currentTarget.reset();
   };
 
   const changeStatusHandler = () => {
@@ -101,11 +78,35 @@ function addButtonListener(item) {
     }
   };
 
-  form.addEventListener('submit', handleSubmit);
   deleteButton.addEventListener('click', deleteButtonHandler);
   editButton.addEventListener('click', editButtonHandler);
   doneButton.addEventListener('change', changeStatusHandler);
 }
+
+const editButtonHandler = () => {
+  modal.style.display = 'block';
+};
+
+const handleSubmit = event => {
+  const TODOHeadline = item.querySelector('h2');
+  const TODOText = item.querySelector('p');
+  const editHeadline = document.getElementById('edit_title');
+  const editText = document.getElementById('edit_todo');
+  editHeadline.value = TODOHeadline.innerHTML;
+  editText.value = TODOText.textContent;
+  event.preventDefault();
+
+  const {
+    elements: { editTitle, editTodoText }
+  } = event.currentTarget;
+
+  TODOHeadline.innerHTML = editTitle.value;
+  TODOText.textContent = editTodoText.value;
+  modal.style.display = 'none';
+  event.currentTarget.reset();
+};
+
+form.addEventListener('submit', handleSubmit);
 
 const itemsToDo = Array.from(document.querySelectorAll('li'));
 
