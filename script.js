f44df738f645b2530148112d8adedf1b29c4071d
Nastya Boiko <boiko.nastasiia@gmail.com>
2022-10-02 13:59:03 +0400

    nothing works

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const headlineText = document.getElementById('headline_text');
 const todoBody = document.getElementById('todo_body');
 const addNewTodoButton = document.getElementById('add_new_todo');
 const todoList = document.getElementById('todo_list');
-const modal = document.getElementById('modal');
+const modal = document.getElementById('modal_wrapper');
 
 addNewTodoButton.addEventListener('click', () => {
   const headlineTodo = headlineText.value || 'Title';
@@ -61,7 +61,12 @@ const addDate = () => {
 function addButtonListener(item) {
   const doneButton = item.querySelector('.finish_button');
   const deleteButton = item.querySelector('.delete_button');
-  const editButton = item.querySelector('.editbutton');
+  const editButton = item.querySelector('.edit_button');
+  const TODOHeadline = item.querySelector('h2');
+  const TODOText = item.querySelector('p');
+  const editHeadline = document.getElementById('edit_title');
+  const editText = document.getElementById('edit_todo');
+  const form = document.querySelector('.modal');
 
   const deleteButtonHandler = () => {
     doneButton.removeEventListener('click', changeStatusHandler);
@@ -70,7 +75,22 @@ function addButtonListener(item) {
   };
 
   const editButtonHandler = () => {
-    modal.style.display = block;
+    modal.style.display = 'block';
+    editHeadline.value = TODOHeadline.innerHTML;
+    editText.value = TODOText.textContent;
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const {
+      elements: { editTitle, editTodoText }
+    } = event.currentTarget;
+
+    TODOHeadline.innerHTML = editTitle.value;
+    TODOText.textContent = editTodoText.value;
+    modal.style.display = 'none';
+    event.currentTarget.reset();
   };
 
   const changeStatusHandler = () => {
@@ -80,10 +100,10 @@ function addButtonListener(item) {
       item.classList.remove('finished_item');
     }
   };
+
+  form.addEventListener('submit', handleSubmit);
   deleteButton.addEventListener('click', deleteButtonHandler);
-
   editButton.addEventListener('click', editButtonHandler);
-
   doneButton.addEventListener('change', changeStatusHandler);
 }
 
